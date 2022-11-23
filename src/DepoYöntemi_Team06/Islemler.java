fa9b0f8a0f5f45b4e6f1b9c5b7af1a54615fbc9f
eminesimsek01 <eminesarikaya09@gmail.com>
2022-11-23 13:35:25 +0300

    23.11 güncelleme ilk

---
+++ src/DepoYöntemi_Team06/Islemler.java
@@ -0,0 +1,23 @@
+package DepoYöntemi_Team06;
+
+import java.util.Scanner;
+
+public class Islemler {
+    static Scanner scan=new Scanner(System.in);
+    public static int tryCatch(){
+
+
+
+        int a;
+        while (true){
+            try {
+                a= scan.nextInt();break;
+            } catch (Exception e) {
+                System.out.println("Lütfen rakamsal değer giriniz");
+                scan.nextLine();
+            }
+        }
+
+        return a;
+    }
+}
