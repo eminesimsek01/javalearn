c3545c52ad3bdf8b82e864311dabc5e202cecb13
eminesimsek01 <eminesarikaya09@gmail.com>
2022-11-25 12:35:33 +0300

    25.11 güncelleme ilk

--- src/j36_Map/Task02.java
+++ src/j36_Map/Task02.java
@@ -4,37 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Task02 {
-    /*
-    Task->
-    Key: isim, email, adres, telefon
-    Key'lere ait value'ları depolayan
-     */
     public static void main(String[] args) {
-        Map<String, String> nazımKrtvzt = new HashMap<>();
-        nazımKrtvzt.put("isim", "Nazım");
-        nazımKrtvzt.put("email", "ebik@gabık.com");
-        nazımKrtvzt.put("adres", "büyük dut ağacının altı");
-        nazımKrtvzt.put("telefon", "1456789");
+        /*
+        Task ->
+         Key:isim ,email,adres,telefon
+         Key2lere ait valueeleri depolayan bir kartvizit  map ve isim Key'e kartvizit map'i value tanımlayan farklı bir map tanımlayıp
+         print eden code create ediniz
+         */
 
-        Map<String, String> mehmetKrtvzt = new HashMap<>();
-        mehmetKrtvzt.put("isim", "mehmet");
-        mehmetKrtvzt.put("email", "ebik@gabıkcom");
-        mehmetKrtvzt.put("adres", "büyük elma ağacının altı");
-        mehmetKrtvzt.put("telefon", "123456890");
 
-        Map<String, Map<String, String>> kartvizit = new HashMap<>();
-        kartvizit.put("QA Nazım", nazımKrtvzt);
-        kartvizit.put("Dev Mehmet", mehmetKrtvzt);
-        System.out.println("kartvizit = " + kartvizit);
-
-        System.out.println("kartvizit.get(\"Dev Mehmet\").get(\"telefon\") = " + kartvizit.get("Dev Mehmet").get("telefon"));
-        Map<Integer, Kartvizit> krt = new HashMap<>();
-        Kartvizit k1 = new Kartvizit("Enise", "enise@tester.com", "google company", 1905);
-        krt.put(k1.id, k1);
-        System.out.println("krt = " + krt);
-        System.out.println("krt.get(k1.id) = " + krt.get(k1.id));//krt.get(k1.id) = Kartvizit{isim='Enise', email='enise@tester.com', telefon='google company', id=1905}
-        System.out.println("krt.get(101) = " + krt.get(1905));//krt.get(101) = Kartvizit{isim='Enise', email='enise@tester.com', telefon='google company', id=1905}
     }
 }
 
 
+//semihMap.get(isim) = Semih Nerde
+//        semihMap.get(telefon) = 0 532 230000
+//        ismailKartvizit.get(isim) = İsmail Demir
+//        ismailKartvizit.get(telefon) = 0 546 450000
+//        semih in telefonu = 0 532 230000
+//        ismail in bilgileri = {telefon=0 546 450000, adres=Amsterdam/Hollanda, email=ismail@gmail.com, isim=İsmail Demir}
+//        kartvizitler = {ismail={telefon=0 546 450000, adres=Amsterdam/Hollanda, email=ismail@gmail.com, isim=İsmail Demir}, semih={telefon=0 532 230000, adres=Frkanfurt/Almanya, email=Semih@hotmail.com, isim=Semih Nerde}}
